6a63d78fe5d9149bf7a49efc6fe53b7bd53500ba
Mercer Denholm <mercer233@gmail.com>
2019-12-13 21:48:27 -0800

    moved client into server folder and added build to git

--- server/app.js
+++ server/app.js
@@ -52,9 +52,9 @@ app.use('/api/url', require('./routes/urls'));
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
   //Set static folder
-  app.use(express.static('../client/build'));
+  app.use(express.static('./client/build'));
   app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '..','client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
   })
 }
 
